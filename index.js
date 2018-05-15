12001eaa7c01fa4bae0a4dfbe2cb8b2897898428
ivanturianytsia <vanyaturianitsa@gmail.com>
2018-05-16 01:29:26 +0200

    google vision - done

--- index.js
+++ index.js
@@ -1,53 +1,13 @@
-const AWS = require('aws-sdk')
 const express = require('express')
 const fileUpload = require('express-fileupload')
+
+const config = require('./lib/config.js')
+const handler = require('./lib/handler.js')
+
 const app = express()
-const path = require('path')
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY
-})
-const S3 = new AWS.S3()
-const bucketName = process.env.AWS_BUCKET
- 
-// default options
+
 app.use(fileUpload())
- 
-app.post('/upload', async (req, res) => {
-  if (!req.files) {
-    return res.status(400).send('No files were uploaded.')
-  }
 
-  // The name of the input field (i.e. "file") is used to retrieve the uploaded file
-  let file = req.files.file
+app.post('/upload', handler.upload)
 
-  try {
-    const s3res = await S3.upload({
-      Bucket: bucketName, // your s3 bucket name
-      Key: `images/${file.name}`,
-      Body: file.data, // concatinating all chunks
-      ACL: 'public-read'
-    }).promise()
-    res.send({
-      data: s3res,
-      status: 'success',
-      msg: 'Image successfully uploaded.'
-    })
-  } catch (err) {
-    res.send({
-      err,
-      status: 'error'
-    })
-  }
-
-  // // Use the mv() method to place the file somewhere on your server
-  // file.mv(path.join(__dirname, file.name), function(err) {
-  //   if (err) {
-  //     return res.status(500).send(err)
-  //   }
-  // })
-})
-
-app.listen(3000, function () {
-  console.log('Your app is up and running on port http://localhost:3000')
-})
+app.listen(3000, () => console.log('App is up and running on port http://localhost:3000'))
