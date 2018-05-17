3e23771121f8e0287ef8bf217c6d739d6b66d4f8
ivanturianytsia <vanyaturianitsa@gmail.com>
2018-05-17 22:07:41 +0200

    cors

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
+const cors = require('cors')
 
 const config = require('./lib/config.js')
 const handler = require('./lib/handler.js')
 
 const app = express()
 
+app.use(cors())
+
 app.use(fileUpload())
 
+app.get('/', handler.list)
 app.post('/upload', handler.upload)
 
-app.listen(3000, () => console.log('App is up and running on port http://localhost:3000'))
+app.listen(config.port, () => console.log(`App is up and running on port http://localhost:${config.port}`))
